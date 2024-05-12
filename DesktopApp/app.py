b65bae53cc6df943ebca78441ffd9bc8aaf5430c
Egor <57009978+trueuser3@users.noreply.github.com>
2024-05-12 23:55:11 +0300

    Create app.py

---
+++ DesktopApp/app.py
@@ -0,0 +1,109 @@
+from PyQt5.QtCore import QDir, Qt, QUrl
+from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
+from PyQt5.QtMultimediaWidgets import QVideoWidget
+from PyQt5.QtWidgets import (QApplication, QFileDialog, QHBoxLayout, QLabel,
+        QPushButton, QSizePolicy, QSlider, QStyle, QVBoxLayout, QWidget)
+from PyQt5.QtWidgets import QMainWindow,QWidget, QPushButton, QAction
+from PyQt5.QtGui import QIcon
+
+import sys
+import torch
+import cv2
+from transformers import ViTImageProcessor
+from transformers import ViTForImageClassification
+from ultralytics import YOLO
+from PIL import Image
+from pipeline_for_app import process_video_with_tracking
+
+
+class VideoWindow(QMainWindow):
+
+    def __init__(self, parent=None):
+        super(VideoWindow, self).__init__(parent)
+        self.setWindowTitle("VideoPlayer")
+
+        self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
+
+        videoWidget = QVideoWidget()
+
+        self.playButton = QPushButton()
+        self.playButton.setEnabled(False)
+        self.playButton.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
+        self.playButton.clicked.connect(self.play)
+
+
+        self.errorLabel = QLabel()
+        self.errorLabel.setSizePolicy(QSizePolicy.Preferred,
+                QSizePolicy.Maximum)
+
+        openAction = QAction(QIcon('open.png'), '&Open', self)
+        openAction.setStatusTip('Open movie')
+        openAction.triggered.connect(self.openFile)
+
+        exitAction = QAction(QIcon('exit.png'), '&Exit', self)
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(self.exitCall)
+
+        menuBar = self.menuBar()
+        fileMenu = menuBar.addMenu('&File')
+        fileMenu.addAction(openAction)
+        fileMenu.addAction(exitAction)
+
+        wid = QWidget(self)
+        self.setCentralWidget(wid)
+
+        controlLayout = QHBoxLayout()
+        controlLayout.addWidget(self.playButton)
+
+        layout = QVBoxLayout()
+        layout.addWidget(videoWidget)
+        layout.addLayout(controlLayout)
+        layout.addWidget(self.errorLabel)
+
+        wid.setLayout(layout)
+
+        self.mediaPlayer.setVideoOutput(videoWidget)
+        self.mediaPlayer.stateChanged.connect(self.mediaStateChanged)
+
+    def openFile(self):
+        fileName, _ = QFileDialog.getOpenFileName(self, "Open Movie",
+                QDir.homePath())
+
+        print('\n')
+        print(fileName)
+        print('\n')
+        
+        model = YOLO('yolov8n.pt')
+        model.fuse()
+        process_video_with_tracking(model, input_video_path = fileName, show_video=False, save_video=True, output_video_path = 'OutputVideo/output_video.mp4') 
+
+        fileName = '/home/trueuser/Desktop/Project/App/OutputVideo/' + 'output_video.mp4'
+        if fileName != '':
+            self.mediaPlayer.setMedia(
+                    QMediaContent(QUrl.fromLocalFile(fileName)))
+            self.playButton.setEnabled(True)
+
+    def exitCall(self):
+        sys.exit(app.exec_())
+
+    def play(self):
+        if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
+            self.mediaPlayer.pause()
+        else:
+            self.mediaPlayer.play()
+
+    def mediaStateChanged(self, state):
+        if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
+            self.playButton.setIcon(
+                    self.style().standardIcon(QStyle.SP_MediaPause))
+        else:
+            self.playButton.setIcon(
+                    self.style().standardIcon(QStyle.SP_MediaPlay))
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    player = VideoWindow()
+    player.resize(800, 600)
+    player.show()
+    sys.exit(app.exec_())
